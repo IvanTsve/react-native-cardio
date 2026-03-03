95dddde92addf5625f398bc881420fd77c649145
IvanTsve <iv_plamenovv@abv.bg>
2026-03-03 19:19:19 +0200

    Update Login.jsx

--- sofia-transport/src/components/auth/login/Login.jsx
+++ sofia-transport/src/components/auth/login/Login.jsx
@@ -1,9 +1,39 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-export default function LoginScreen() { 
+import { View, Text, TextInput } from 'react-native';
+
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { ImageBackground } from 'react-native-web';
+
+
+export default function LoginScreen() {
+
     return (
-        <View>
-            <Text>Loginddd</Text>
+        <View style={global.commonStyles.container}>
+            <View style={{ alignItems: 'center', marginTop: 40, marginBottom:20 }}>
+                <View style={{ backgroundColor: '#93c5fd', borderRadius: 85, padding: 15, marginBottom: 20 }}>
+                    <MaterialCommunityIcons name="bus" size={150} color="#1d4ed8" />
+                </View>
+                <Text numberOfLines={1} style={global.commonStyles.title}>Sofia Transport</Text>
+            </View>
+            <View style={[global.commonStyles.inputWrapper, { marginLeft: 20, gap: 15 }]}>
+                <View>
+                    <Text style={global.commonStyles.inputLabel}>Email</Text>
+                    <TextInput
+                        style={global.commonStyles.input}
+                        placeholder="you@example.com"
+                        placeholderTextColor="#93c5fd"
+                    />
+                </View>
+                <View>
+                    <Text style={global.commonStyles.inputLabel}>Password</Text>
+                    <TextInput
+                        secureTextEntry={true}
+                        placeholder="password"
+                        placeholderTextColor="#93c5fd"
+                        style={global.commonStyles.input}
+                    />
+                </View>
+            </View>
         </View>
-    )
+    );
 }
