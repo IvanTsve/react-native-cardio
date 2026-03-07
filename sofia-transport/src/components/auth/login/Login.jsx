de68f64c4dbd0b5c78fe2821cc035c0a8a6171d2
IvanTsve <iv_plamenovv@abv.bg>
2026-03-07 17:42:57 +0200

    added scrollview

--- sofia-transport/src/components/auth/login/Login.jsx
+++ sofia-transport/src/components/auth/login/Login.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, ScrollView } from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
-import { useNavigation } from '@react-navigation/native'; 
+import { useNavigation } from '@react-navigation/native';
 
 export default function LoginScreen() {
-    const navigation =  useNavigation();
+    const navigation = useNavigation();
 
     function signIn() {
 
@@ -13,53 +13,56 @@ export default function LoginScreen() {
 
     return (
         <View style={global.commonStyles.container}>
-            <View style={{ alignItems: 'center', marginTop: 40, marginBottom: 20 }}>
-                <View style={global.commonStyles.iconShadow}>
-                    <MaterialCommunityIcons name="bus" size={150} color="#1d4ed8" />
-                </View>
-                <Text numberOfLines={1} style={global.commonStyles.title}>Sofia Transport</Text>
-            </View>
-            <View style={[global.commonStyles.inputWrapper, { marginLeft: 20, gap: 15, backgroundColor: 'white', padding: 20, borderRadius: 35 }]}>
-                <View>
-                    <Text style={global.commonStyles.inputLabel}>Email</Text>
-                    <TextInput
-                        style={global.commonStyles.input}
-                        placeholder="you@example.com"
-                        placeholderTextColor="#93c5fd"
-                    />
-                </View>
-                <View>
-                    <Text style={global.commonStyles.inputLabel}>Password</Text>
-                    <TextInput
-                        secureTextEntry={true}
-                        placeholder="password"
-                        placeholderTextColor="#93c5fd"
-                        style={global.commonStyles.input}
-                    />
-                </View>
-                <View >
-                    <TouchableOpacity
-                        style={global.commonStyles.fancyButton}
-                        onPress={signIn}
-                    >
-                        <Text style={{ color: 'white', fontWeight: '600', fontSize: 16 }}>
-                            Sign In
-                        </Text>
-                    </TouchableOpacity>
-                </View>
+            <ScrollView>
 
-                <View>
-                    <Text>
-                        Don't have an account?{' '}
-                        <Text
-                            style={{ color: '#1d4ed8', fontWeight: 'bold' }}
-                            onPress={() => navigation.navigate('Register')}
-                        >
-                            Create Account
-                        </Text>
-                    </Text>
+                <View style={{ alignItems: 'center', marginTop: 40, marginBottom: 20 }}>
+                    <View style={global.commonStyles.iconShadow}>
+                        <MaterialCommunityIcons name="bus" size={150} color="#1d4ed8" />
+                    </View>
+                    <Text numberOfLines={1} style={global.commonStyles.title}>Sofia Transport</Text>
                 </View>
-            </View>
+                <View style={[global.commonStyles.inputWrapper, { marginLeft: 20, gap: 15, backgroundColor: 'white', padding: 20, borderRadius: 35 }]}>
+                    <View>
+                        <Text style={global.commonStyles.inputLabel}>Email</Text>
+                        <TextInput
+                            style={global.commonStyles.input}
+                            placeholder="you@example.com"
+                            placeholderTextColor="#93c5fd"
+                        />
+                    </View>
+                    <View>
+                        <Text style={global.commonStyles.inputLabel}>Password</Text>
+                        <TextInput
+                            secureTextEntry={true}
+                            placeholder="password"
+                            placeholderTextColor="#93c5fd"
+                            style={global.commonStyles.input}
+                        />
+                    </View>
+                    <View >
+                        <TouchableOpacity
+                            style={global.commonStyles.fancyButton}
+                            onPress={signIn}
+                        >
+                            <Text style={{ color: 'white', fontWeight: '600', fontSize: 16 }}>
+                                Sign In
+                            </Text>
+                        </TouchableOpacity>
+                    </View>
+
+                    <View>
+                        <Text>
+                            Don't have an account?{' '}
+                            <Text
+                                style={{ color: '#1d4ed8', fontWeight: 'bold' }}
+                                onPress={() => navigation.navigate('Register')}
+                            >
+                                Create Account
+                            </Text>
+                        </Text>
+                    </View>
+                </View>
+            </ScrollView>
         </View>
     );
 }
