f31fd3eeef1ff8dc93efbf68b0097abcd98a088c
IvanTsve <iv_plamenovv@abv.bg>
2026-03-07 18:12:34 +0200

    Create Map.jsx

---
+++ sofia-transport/src/components/map/Map.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text, TextInput, TouchableOpacity, ScrollView } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+export default function Map() {
+
+
+    return(
+        <View>
+            <Text>this is map Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta animi voluptatum porro quas quia quam earum nulla voluptate, ut ratione.</Text>
+        </View>
+    )
+}
