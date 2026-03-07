74229ce02775827d512546cab9f34466bf753376
IvanTsve <iv_plamenovv@abv.bg>
2026-03-07 22:34:47 +0200

    Update apiRequest.js

--- sofia-transport/src/services/apiRequest.js
+++ sofia-transport/src/services/apiRequest.js
@@ -3,8 +3,6 @@ import axios from 'axios';
 const API_URL = 'https://gtfs.sofiatraffic.bg/api/v1';
 
 export default function apiRequest(url, method, data = null) {
-    console.log(API_URL+url);
-
     return axios.request({
         url: API_URL+url,
         method: method,
