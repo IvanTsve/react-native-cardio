f2ef1d92308ba3766768c72c57e64c7af7aa1566
IvanTsve <iv_plamenovv@abv.bg>
2026-03-07 18:12:13 +0200

    Create Profile.jsx

---
+++ sofia-transport/src/components/auth/user/Profile.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text, TextInput, TouchableOpacity, ScrollView } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+export default function Profile() {
+
+
+    return(
+        <View>
+            <Text>this is profile Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus vitae, iste numquam dolores odio commodi ullam maiores sapiente perferendis nemo.</Text>
+        </View>
+    )
+}
